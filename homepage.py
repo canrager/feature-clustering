654185a3edd6794ceebe3a646cddf360bc1cec88
Can Rager <web.rager@posteo.de>
2024-01-24 15:24:23 -0800

    add requirements

--- homepage.py
+++ homepage.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import seaborn as sns
-import plotly.express as px
-import matplotlib.pyplot as plt
 import json
 import os
 from collections import defaultdict
